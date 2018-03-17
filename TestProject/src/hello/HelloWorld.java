3a735aafaded0eba589853c0e064b3e25ff3f572
Vibin <Vibin@vibin>
2018-03-17 08:51:26 +0000

    Added new print

--- TestProject/src/hello/HelloWorld.java
+++ TestProject/src/hello/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("hello world");
+		
+		System.out.println("hello vibin");
+		
+		System.out.println("hello vibin1");
 
 	}
 
